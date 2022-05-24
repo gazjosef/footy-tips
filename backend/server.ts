31269a64a39226baa708724fc8c79e1bf0ae8177
Gareth Hind <garethjhind@gmail.com>
2022-05-24 12:00:59 +1000

    Connect w/Mongoose

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from "express";
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5001;
+
+connectDB();
 
 const app = express();
 
