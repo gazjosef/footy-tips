d6b761cb2af15787bdf8e53714f21c98882ad952
Gareth Hind <garethjhind@gmail.com>
2023-04-28 19:06:59 +1000

    Added Routes

--- backend/routes/tipRoutes.js
+++ backend/routes/tipRoutes.js
@@ -13,6 +13,18 @@ router.get("/", (req, res) => {
   res.status(200).json({ message: "Get Tips" });
 });
 
+router.post("/", (req, res) => {
+  res.status(200).json({ message: "Create Tips" });
+});
+
+router.put("/:id", (req, res) => {
+  res.status(200).json({ message: `Update Tip ${req.params.id}` });
+});
+
+router.delete("/:id", (req, res) => {
+  res.status(200).json({ message: `Delete Tips ${req.params.id}` });
+});
+
 // // CREATE
 // router.post("/", createTip);
 
