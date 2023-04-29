50c97ced2b2e0e28d4db0fac3acab5ef9c64c84d
Gareth Hind <garethjhind@gmail.com>
2023-04-29 14:08:28 +1000

    Connect w/Mongoose

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5001;
 
-// connectDB();
+connectDB();
 // const path = require("path");
 
 const app = express();
