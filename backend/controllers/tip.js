fda69a77d36097bbe949cf9670ebee7423c78345
Gareth Hind <garethjhind@gmail.com>
2023-04-29 21:56:26 +1000

    Delete Tip

--- backend/controllers/tip.js
+++ backend/controllers/tip.js
@@ -1,9 +1,9 @@
 const asyncHandler = require("express-async-handler");
 const Tip = require("../models/tipModel");
 
-// CREATE TIP
-// ROUTE: POST /api/tips
-// ACCESS: Private
+// * CREATE TIP
+// * ROUTE: POST /api/tips
+// * ACCESS: Private
 const createTip = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
@@ -18,9 +18,9 @@ const createTip = asyncHandler(async (req, res) => {
   res.status(200).json(tip);
 });
 
-// UPDATE TIP
-// ROUTE: POST /api/tips/:id
-// ACCESS: Private
+// * UPDATE TIP
+// * ROUTE: POST /api/tips/:id
+// * ACCESS: Private
 const updateTip = asyncHandler(async (req, res, next) => {
   const tip = await Tip.findById(req.params.id);
 
@@ -36,9 +36,9 @@ const updateTip = asyncHandler(async (req, res, next) => {
   res.status(200).json(updatedTip);
 });
 
-// DELETE TIP
-// ROUTE: DELETE /api/tips/:id
-// ACCESS: Private
+// * DELETE TIP
+// * ROUTE: DELETE /api/tips/:id
+// * ACCESS: Private
 const deleteTip = asyncHandler(async (req, res, next) => {
   const tip = await Tip.findById(req.params.id);
 
@@ -52,31 +52,19 @@ const deleteTip = asyncHandler(async (req, res, next) => {
   res.status(200).json({ id: req.params.id });
 });
 
-// GET TIP
-// ROUTE: GET /api/tips/:id
-// ACCESS: Private
+// * GET TIP
+// * ROUTE: GET /api/tips/:id
+// * ACCESS: Private
 const getTip = asyncHandler(async (req, res, next) => {
   res.status(200).json({ message: `Get Tip ${req.params.id}` });
-  // try {
-  //   const tip = await Tip.find(req.params.id);
-  //   res.status(200).json(tip);
-  // } catch (error) {
-  //   res.status(500).json(error);
-  // }
 });
 
-// GET ALL TIPS
-// ROUTE: GET /api/tips
-// ACCESS: Private
+// * GET ALL TIPS
+// * ROUTE: GET /api/tips
+// * ACCESS: Private
 const getAllTips = asyncHandler(async (req, res, next) => {
   const tips = await Tip.find();
   res.status(200).json(tips);
-  // try {
-  //   const tips = await Tip.find();
-  //   res.status(200).json(tips);
-  // } catch (error) {
-  //   next(error);
-  // }
 });
 
 module.exports = {
