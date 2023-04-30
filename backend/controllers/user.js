2ad148230cf60507e908f0da9177e15783c0c903
Gareth Hind <garethjhind@gmail.com>
2023-04-30 16:11:16 +1000

    Protecting Routes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -72,7 +72,13 @@ const loginUser = asyncHandler(async (req, res) => {
 // ROUTE: GET /api/users/me
 // ACCESS: Private
 const getMe = asyncHandler(async (req, res) => {
-  res.json({ message: "User data display" });
+  const { _id, name, email } = await User.findById(req.user.id);
+
+  res.status(200).json({
+    id: _id,
+    name,
+    email,
+  });
 });
 
 const generateToken = (id) => {
